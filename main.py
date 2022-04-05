aabb037e3e66ab7d91fb40cfcf587d84833c5060
darshb <darshb@deloitte.com>
2022-04-05 15:02:57 +0530

    Mini assignment1 (Searching of common elements added from both the strings)

--- main.py
+++ main.py
@@ -26,6 +26,35 @@ class PairsPossible(StringClass):
         print(*combos)
 
 
-Demo = PairsPossible("12314532")
+class SearchCommonElements:
+
+    def __init__(self, a, b):
+        self.StringClass_string = a
+        self.PossiblePair_string = b
+        self.common_list = list()
+
+    def find_common(self):
+        dict = {}
+
+        for ele in self.StringClass_string:
+            if ele in self.PossiblePair_string:
+                if ele in dict:
+                    continue
+                else:
+                    dict[ele] = 1
+
+        for key in dict:
+            self.common_list.append(key)
+
+        return self.common_list
+
+
+obj = StringClass("12314532")
+print(obj.strlen())
+print(obj.str_list())
+
+Demo = PairsPossible("12389764")
 Demo.Possible_pairs()
 
+obj3 = SearchCommonElements(obj.str,Demo.str)
+print(obj3.find_common())
