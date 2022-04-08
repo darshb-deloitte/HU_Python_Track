3f03593a86d2df7671e278a45afb287c3af4ebc3
darshb <darshb@deloitte.com>
2022-04-08 12:09:18 +0530

    Main Assignment( Adding USER AND LOGIN DETAILS)

--- Main Assignment/BasePage.py
+++ Main Assignment/BasePage.py
@@ -1,11 +1,22 @@
 import pandas as pd
 
-movie_file = pd.read_csv("movies.csv")
-admin_detail = pd.read_csv("admin.csv")
-user_detail = pd.read_csv("user.csv")
+try:
+    movie_file = pd.read_csv("movies.csv")
+except Exception:
+    movie_file = pd.DataFrame()
+
+try:
+    admin_detail = pd.read_csv("admin.csv")
+except Exception:
+    admin_detail = pd.DataFrame()
+
+try:
+    user_detail = pd.read_csv("user.csv")
+except Exception:
+    user_detail = pd.DataFrame()
 
 
-def Register():
+def register_func():
     name = input("Name :")
     email = input("Email :")
     phone = input("Phone :")
@@ -16,7 +27,32 @@ def Register():
     return name, lis
 
 
-def add_edit_new_movie():
+def Edit_movie(title):
+    # title = input("Title [{}] :".format(title))
+    print("Title :", title)
+    genre = input("Genre [{}] :".format(movie_file[title][0]))
+    duration = input("Length [{}] :".format(movie_file[title][1]))
+    cast = input("Cast [{}] :".format(movie_file[title][2]))
+    director = input("Director [{}] :".format(movie_file[title][3]))
+    admin_rating = input("Admin Rating [{}] :".format(movie_file[title][4]))
+    lang = input("Language [{}] :".format(movie_file[title][5]))
+    timing_string = input("Timing [{}] :".format(movie_file[title][6]))
+    timing = list(timing_string.split(","))
+    # no_of_shows = input("Number of shows :")
+    no_of_shows = len(timing)
+    print("Number of shows in a day :", no_of_shows)
+    first_show = input("First Show [{}] :".format(movie_file[title][8]))
+    interval_time = input("Interval Time [{}] :".format(movie_file[title][9]))
+    gap_btw_show = input("Gap Between Shows [{}] :".format(movie_file[title][10]))
+    capacity = input("Capacity [{}] :".format(movie_file[title][11]))
+
+    lis = [genre, duration, cast, director, admin_rating, lang, timing, no_of_shows, first_show, interval_time,
+           gap_btw_show, capacity]
+
+    return title, lis
+
+
+def Add_movie():
     title = input("Title :")
     genre = input("Genre :")
     duration = input("Length :")
@@ -24,8 +60,11 @@ def add_edit_new_movie():
     director = input("Director :")
     admin_rating = input("Admin Rating :")
     lang = input("Language :")
-    timing = input("Timing :")
-    no_of_shows = input("Length :")
+    timing_string = input("Timing :")
+    timing = list(timing_string.split(","))
+    # no_of_shows = input("Number of shows :")
+    no_of_shows = len(timing)
+    print("Number of shows in a day:", no_of_shows)
     first_show = input("First Show :")
     interval_time = input("Interval Time :")
     gap_btw_show = input("Gap Between Shows :")
@@ -37,29 +76,51 @@ def add_edit_new_movie():
     return title, lis
 
 
-def login(user, password):
-    dictionary = dict()
-    dictionary["admin"] = "password"
-    dictionary["user"] = "pass"
+def login_func(user, password):
+    admins = list(admin_detail.columns)
+    users = list(user_detail.columns)
 
-    if dictionary[user] == password:
-        return "admin"
-    elif (user in dictionary) and (dictionary[user] == password):
+    if (user in users) and (user_detail[user][3] == password):
         return "user"
+
+    elif (user in admins) and (admin_detail[user][0] == password):
+        return "admin"
+
     else:
         return "invalid"
 
-def userdisplay(title):
+
+def user_display(title):
     lis = list(movie_file[title])
 
-    print("Title :", title , "\n"
-          "Genre :", lis[0], "\n"
-          "Length", lis[1], "\n"
-          "Cast :", lis[2], "\n"
-          "Director :", lis[3], "\n"
-          "Admin :", lis[4], "\n"
-          "Timings :", lis[5], "\n"
-          "User Rating :", lis[6])
+    print("Title :", title, "\n"
+                            "Genre :", lis[0], "\n"
+                                               "Length", lis[1], "\n"
+                                                                 "Cast :", lis[2], "\n"
+                                                                                   "Director :", lis[3], "\n"
+                                                                                                         "Admin :",
+          lis[4], "\n"
+                  "Timings :", lis[5], "\n"
+                                       "User Rating :", lis[6])
 
-def Book_ticket(title):
+    func = int(input("1. Book Tickets \n"
+                     "2. Cancel Tickets \n"
+                     "3. Give User Rating"))
+
+    return func
+
+
+def ratings():
+    movies = ("RRR", "83", "RX100")
+    for movie in movies:
+        print("Rating For Movie:", movie)
+        for i in range(len(movies)):
+            rating = int(input("Enter Movie rating: "))
+            if rating < 1 or rating > 10:
+                print("That's not a Valid number")
+                continue
+            break
+
+
+def book_ticket(title):
     lis = list(movie_file[title])
